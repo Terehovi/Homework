22716e867dd72ee8417675258e8b962fb7792773
Igor <terehovi303@gmail.com>
2023-01-17 21:56:03 +0300

    Написал коды для 13 и 15 задач

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите целое число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int ThreeDigits(int number)
+{
+    if (number / 100 != 0) return 1;
+    else return 0;
+}
+int threeDigits = ThreeDigits(num);
+
+while (num / 1000 != 0)
+{
+    num /= 10;
+}
+
+if (threeDigits == 0) Console.WriteLine("Третьей цифры нет");
+else
+{
+    num %= 10;
+    Console.WriteLine(num);
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите цифру то 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+// if ((num == 6) || (num == 7))
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+bool DayOfWeek(int number)
+{
+    return ((num == 6) || (num == 7));
+}
+
+bool dayOfWeek = DayOfWeek(num);
+if (dayOfWeek == true) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
