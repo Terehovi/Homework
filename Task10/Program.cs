ac866ff4d3873f8aa66f39ff2e2d28a27b99049a
Igor <terehovi303@gmail.com>
2023-01-17 20:06:25 +0300

    Написал код для 10 задачи

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое трёхзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit(int number)
+{
+    number /= 10;
+    number %= 10;
+    return number;
+}
+
+int secondDigit = SecondDigit(num);
+Console.WriteLine(secondDigit);
