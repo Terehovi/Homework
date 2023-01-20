eb125c5a54f138d9b3de5561e0a778256d85d2b4
Igor <terehovi303@gmail.com>
2023-01-20 13:15:21 +0300

    Написал код для 23 задачи

---
+++ Task23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) Console.WriteLine("Некорректный ввод");
+else TableCubes(number);
+
+
+void TableCubes(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count, 5}   {count * count * count, 5}");
+        count++;
+    }
+}
