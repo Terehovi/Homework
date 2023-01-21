30df9d16e277b05c89e5332e695a601e173aea10
Igor <terehovi303@gmail.com>
2023-01-21 12:01:36 +0300

    написал код для 19 задачи

---
+++ Task19/Program.cs
@@ -0,0 +1,16 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 10000 && num <= 99999)
+{
+    if (num / 10000 == num % 10)
+    {
+        num /= 10;
+        num %= 1000;
+        if (num / 100 == num % 10)
+        {
+            Console.WriteLine("Да");
+        }
+        else Console.WriteLine("Нет");
+    }
+    else Console.WriteLine("Нет");
+} 
+else Console.WriteLine("Некоректный ввод");
