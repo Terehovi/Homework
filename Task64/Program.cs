c03053a8c1e3497be37a160acc81a0a98a33647d
Igor <terehovi303@gmail.com>
2023-02-14 09:11:07 +0300

    написал код для 64, 66, 68 задач

---
+++ Task64/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(number);
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите 1 натуральное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2 натуральное число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = SummOfNumbers(number1, number2);
+Console.WriteLine(result);
+
+int SummOfNumbers(int num1, int num2)
+{
+    if (num1 > num2) return 0;
+    return num1 + SummOfNumbers(num1 + 1, num2);
+}
